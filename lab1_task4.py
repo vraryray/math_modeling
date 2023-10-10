75ca5e11d08b653257862537fe101d657e329fc8
vraryray <146106222+vraryray@users.noreply.github.com>
2023-10-10 16:43:46 +0000

    commit

--- lab1_task4.py
+++ lab1_task4.py
@@ -1,2 +1,7 @@
 a = [1, 5, 'Good', 'Bad']
 b = [9, 'Blue', 'Red', 11]
+print(str(a[1]) + str(b[3]))
+print(str(a[2]) + str(b[2]))
+print(a[0] * b[0])
+print(a[1] ** b[3])
+print(a + b)
---
+++ lab1_task6.py
@@ -0,0 +1,7 @@
+a = input('Возраст: ')
+b = input('Пол: ')
+c = input('Имя: ')
+d = input('Город: ')
+e = input('В каком классе учитесь: ')
+list = [a, b, c, d, e]
+print(list)
