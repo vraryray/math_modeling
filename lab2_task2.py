a5f8149c7ba4a59c52ed009ce3638c24a2cc3d88
vraryray <146106222+vraryray@users.noreply.github.com>
2023-10-17 15:59:24 +0000

    commit

---
+++ lab2_task2.py
@@ -0,0 +1,7 @@
+b1 = int(input())
+q = int(input())
+n = int(input())
+b = [ ]
+for i in range(n):
+    b.append(i ** q) 
+print(b)    
---
+++ lab2_task4.py
@@ -0,0 +1,4 @@
+n = int(input())
+f = [1, 1]
+for i in range(n):
+    
