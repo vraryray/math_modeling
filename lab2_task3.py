294638dbe311a01c41bc4e0a2069a59f2ff60331
vraryray <146106222+vraryray@users.noreply.github.com>
2023-10-17 14:40:59 +0000

    commit

--- lab2_task3.py
+++ lab2_task3.py
@@ -1,7 +1,5 @@
 year = int(input())
-if year % 4 == 0:
-    print('Високосноый год')
-elif year % 4 == 0 and year % 100 != 0:
+if year % 4 == 0 and year % 100 != 0 and year % 400 == 0:
     print('Високосноый год')
 else:
     print('Невисокосный год')    
